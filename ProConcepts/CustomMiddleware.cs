a1ebf93d9692bf6549b75f388e30dbee274e5f93
pooja <115543435+poojaparasharInfrablok@users.noreply.github.com>
2022-12-17 13:31:38 +0530

    Part 8

---
+++ ProConcepts/CustomMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProConcepts
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync("Called from CustomMiddleware. \n");
+            await _next(httpContext);
+        }
+    }
+}
---
+++ ProConcepts/MiddlewareExtension.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProConcepts
+{
+    public static class MiddlewareExtension
+    {
+        public static void MyExtensionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<CustomMiddleware>();
+            app.MapWhen(context => context.Request.Query.ContainsKey("branch"), handleBranchApp =>
+            {
+                handleBranchApp.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Branch Map When \n");
+                });
+            });
+            //Handle Multiple Segements
+            app.Map("/Map1/Seg1", HandleMultiSegApp =>
+            {
+                HandleMultiSegApp.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Map multi seg \n");
+                });
+            });
+
+            app.Map("/MainMapPath", MainApp =>
+            {
+                MainApp.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("Main \n");
+                    await next.Invoke();
+                });
+                MainApp.Map("/NestedMapPath", NestedApp =>
+                {
+                    NestedApp.Use(async (context, next) =>
+                    {
+                        await context.Response.WriteAsync("Nested \n");
+                        await next.Invoke();
+                    });
+                });
+            });
+
+            app.Map("/map1", mapappbuilder => {
+                mapappbuilder.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Map1 \n");
+                });
+            });
+            app.UseWhen(context => context.Request.Query.ContainsKey("branch"), myconappbuilder =>
+            {
+                myconappbuilder.Use(async (context, next) => {
+                    await context.Response.WriteAsync("11 \n");
+                    await next.Invoke();
+                    await context.Response.WriteAsync("55 \n");
+                });
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("1 \n");
+                await next.Invoke();
+                await context.Response.WriteAsync("5 \n");
+            });
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("2 \n");
+                await next.Invoke();
+                await context.Response.WriteAsync("4 \n");
+            });
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("3 \n");
+            });
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Called from Run Middleware2 \n");
+            });
+        }
+    }
+}
